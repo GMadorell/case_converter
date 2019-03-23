3c6276e60e79b761b46e83d469701634027acce0
GMadorell <gmadorell@gmail.com>
2019-03-23 17:41:22 +0100

    Add initial scaffolding of the project

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jessevdk/go-flags"
+)
+
+var opts struct {
+	From string `short:"f" long:"from" required:"true" name:"Format to convert from"`
+	To   string `short:"t" long:"to"   required:"true" name:"Format to convert to"`
+}
+
+const help string = `
+Usage: 
+$ case_converter -f format_from -t format_to some_text
+
+Examples:
+$ case_converter -f snake -t camel some_text
+-> someText
+$ case_converter -f pascal -t kebab SomeText
+-> some-text
+
+Format options (accepted by the -f and -t flags):
+* snake    (ej: some_example_sentence)
+* camel    (ej: someExampleSentence)
+* pascal   (ej: SomeExampleSentence)
+* lower    (ej: some example sentence)
+* upper    (ej: SOME EXAMPLE SENTENCE)
+* title    (ej: Some Example Sentence)
+* sentence (ej: Some example sentence)
+* flat     (ej: someexamplesentence)
+* kebab    (ej: some-example-sentence)
+* dot      (ej: some.example.sentence)
+`
+
+func main() {
+	args, err := flags.Parse(&opts)
+
+	if err != nil {
+		fmt.Println(help)
+		log.Fatal(err)
+	}
+
+	if len(args) != 1 {
+		fmt.Println(help)
+		log.Fatal("case_converter accepts a single argument")
+	}
+
+	convertCase(opts.From, opts.To, args[0])
+}
+
+func convertCase(from string, to string, text string) string {
+	fmt.Printf("From: %s, to: %s, text: %s\n", from, to, text)
+	return "Test"
+}
